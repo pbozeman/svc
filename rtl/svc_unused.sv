2d1fcbe077cc28861e0a188e3ec91fd440419a5e
Patrick Bozeman <pbozeman@gmail.com>
2024-12-30 10:09:23 -0800

    [svc] simply SVC_UNUSED

--- rtl/svc_unused.sv
+++ rtl/svc_unused.sv
@@ -12,32 +12,16 @@
 // clock:
 //
 //   `SVC_UNUSED({clk, rst_n, s_axil_araddr[LSB-1:0]});
-//
-// Implementation note: one would think it should be possible to do
-// .WIDTH($bits(signal)) directly when instantiating the module, but
-// iverilog reduced the example above to 1 bit. Making a signal first
-// and then doing a $bits on that intermediate symbol works, whereas
-// doing $bits(signal) directly during module instantiation, did not.
 
-`ifndef FORMAL
 `define SVC_UNUSED(signal)                            \
-  logic [$bits(signal)-1:0] svc_unused_sig_`__LINE__; \
-  svc_unused #(                                       \
-    .WIDTH($bits(svc_unused_sig_`__LINE__))           \
-  ) svc_unused_`__LINE__ (                            \
-    .bits(signal)                                     \
+  svc_unused svc_unused_i (                           \
+    .unused(& signal)                                 \
   );
 
-module svc_unused #(
-    parameter WIDTH
-) (
+module svc_unused (
     // verilator lint_off: UNUSEDSIGNAL
-    input logic [WIDTH-1:0] bits
+    input logic unused
     // verilator lint_on: UNUSEDSIGNAL
 );
 endmodule
-`else
-`define SVC_UNUSED(signal)
-`endif
-
 `endif
