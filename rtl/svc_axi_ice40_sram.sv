bcfc13bf8e822cff96a43ae410db94c421def45d
Patrick Bozeman <pbozeman@gmail.com>
2025-01-05 14:49:41 -0800

    [ice40][axi_sram] add svc_axi_ice40_sram

---
+++ rtl/svc_axi_ice40_sram.sv
@@ -0,0 +1,172 @@
+`ifndef SVC_AXI_ICE40_SRAM_SV
+`define SVC_AXI_ICE40_SRAM_SV
+
+`include "svc.sv"
+`include "svc_axi_sram_if.sv"
+`include "svc_ice40_sram_io_if.sv"
+
+module svc_axi_ice40_sram #(
+    parameter AXI_ADDR_WIDTH  = 20,
+    parameter AXI_DATA_WIDTH  = 16,
+    parameter AXI_ID_WIDTH    = 4,
+    parameter AXI_STRB_WIDTH  = (AXI_DATA_WIDTH / 8),
+    parameter LSB             = $clog2(AXI_DATA_WIDTH) - 3,
+    parameter SRAM_ADDR_WIDTH = AXI_ADDR_WIDTH - LSB,
+    parameter SRAM_DATA_WIDTH = AXI_DATA_WIDTH,
+    parameter SRAM_STRB_WIDTH = AXI_STRB_WIDTH,
+    parameter SRAM_META_WIDTH = AXI_ID_WIDTH
+) (
+    input logic clk,
+    input logic rst_n,
+
+    //
+    // AXI subordinate interface
+    //
+    input  logic                      s_axi_awvalid,
+    output logic                      s_axi_awready,
+    input  logic [  AXI_ID_WIDTH-1:0] s_axi_awid,
+    input  logic [AXI_ADDR_WIDTH-1:0] s_axi_awaddr,
+    input  logic [               7:0] s_axi_awlen,
+    input  logic [               2:0] s_axi_awsize,
+    input  logic [               1:0] s_axi_awburst,
+    input  logic                      s_axi_wvalid,
+    output logic                      s_axi_wready,
+    input  logic [AXI_DATA_WIDTH-1:0] s_axi_wdata,
+    input  logic [AXI_STRB_WIDTH-1:0] s_axi_wstrb,
+    input  logic                      s_axi_wlast,
+    output logic                      s_axi_bvalid,
+    input  logic                      s_axi_bready,
+    output logic [  AXI_ID_WIDTH-1:0] s_axi_bid,
+    output logic [               1:0] s_axi_bresp,
+
+    input  logic                      s_axi_arvalid,
+    output logic                      s_axi_arready,
+    input  logic [  AXI_ID_WIDTH-1:0] s_axi_arid,
+    input  logic [AXI_ADDR_WIDTH-1:0] s_axi_araddr,
+    input  logic [               7:0] s_axi_arlen,
+    input  logic [               2:0] s_axi_arsize,
+    input  logic [               1:0] s_axi_arburst,
+    output logic                      s_axi_rvalid,
+    input  logic                      s_axi_rready,
+    output logic [  AXI_ID_WIDTH-1:0] s_axi_rid,
+    output logic [AXI_DATA_WIDTH-1:0] s_axi_rdata,
+    output logic [               1:0] s_axi_rresp,
+    output logic                      s_axi_rlast,
+
+    //
+    // IO to/from the async sram chip
+    //
+    output logic [SRAM_ADDR_WIDTH-1:0] sram_io_addr,
+`ifndef FORMAL
+    inout  wire  [SRAM_DATA_WIDTH-1:0] sram_io_data,
+`else
+    input  wire  [SRAM_DATA_WIDTH-1:0] sram_io_data,
+`endif
+    output logic                       sram_io_we_n,
+    output logic                       sram_io_oe_n,
+    output logic                       sram_io_ce_n
+);
+  logic                       sram_cmd_valid;
+  logic                       sram_cmd_ready;
+  logic [SRAM_ADDR_WIDTH-1:0] sram_cmd_addr;
+  logic [SRAM_META_WIDTH-1:0] sram_cmd_meta;
+  logic                       sram_cmd_last;
+  logic                       sram_cmd_wr_en;
+  logic [SRAM_DATA_WIDTH-1:0] sram_cmd_wr_data;
+  logic [SRAM_STRB_WIDTH-1:0] sram_cmd_wr_strb;
+  logic                       sram_resp_valid;
+  logic                       sram_resp_ready;
+  logic [SRAM_META_WIDTH-1:0] sram_resp_meta;
+  logic                       sram_resp_last;
+  logic [SRAM_DATA_WIDTH-1:0] sram_resp_rd_data;
+
+  svc_ice40_sram_io_if #(
+      .SRAM_ADDR_WIDTH(SRAM_ADDR_WIDTH),
+      .SRAM_DATA_WIDTH(SRAM_DATA_WIDTH),
+      .SRAM_STRB_WIDTH(SRAM_STRB_WIDTH),
+      .SRAM_META_WIDTH(SRAM_META_WIDTH)
+  ) svc_ice40_sram_io_if_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .sram_cmd_valid  (sram_cmd_valid),
+      .sram_cmd_ready  (sram_cmd_ready),
+      .sram_cmd_addr   (sram_cmd_addr),
+      .sram_cmd_meta   (sram_cmd_meta),
+      .sram_cmd_last   (sram_cmd_last),
+      .sram_cmd_wr_en  (sram_cmd_wr_en),
+      .sram_cmd_wr_data(sram_cmd_wr_data),
+      .sram_cmd_wr_strb(sram_cmd_wr_strb),
+
+      .sram_resp_valid  (sram_resp_valid),
+      .sram_resp_ready  (sram_resp_ready),
+      .sram_resp_meta   (sram_resp_meta),
+      .sram_resp_last   (sram_resp_last),
+      .sram_resp_rd_data(sram_resp_rd_data),
+
+      .sram_io_addr(sram_io_addr),
+      .sram_io_data(sram_io_data),
+      .sram_io_we_n(sram_io_we_n),
+      .sram_io_oe_n(sram_io_oe_n),
+      .sram_io_ce_n(sram_io_ce_n)
+  );
+
+  svc_axi_sram_if #(
+      .AXI_ADDR_WIDTH (AXI_ADDR_WIDTH),
+      .AXI_DATA_WIDTH (AXI_DATA_WIDTH),
+      .AXI_ID_WIDTH   (AXI_ID_WIDTH),
+      .SRAM_ADDR_WIDTH(SRAM_ADDR_WIDTH),
+      .SRAM_DATA_WIDTH(SRAM_DATA_WIDTH),
+      .SRAM_STRB_WIDTH(SRAM_STRB_WIDTH),
+      .SRAM_META_WIDTH(SRAM_META_WIDTH)
+  ) svc_axi_sram_if_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .s_axi_awvalid(s_axi_awvalid),
+      .s_axi_awready(s_axi_awready),
+      .s_axi_awid   (s_axi_awid),
+      .s_axi_awaddr (s_axi_awaddr),
+      .s_axi_awlen  (s_axi_awlen),
+      .s_axi_awsize (s_axi_awsize),
+      .s_axi_awburst(s_axi_awburst),
+      .s_axi_wvalid (s_axi_wvalid),
+      .s_axi_wready (s_axi_wready),
+      .s_axi_wdata  (s_axi_wdata),
+      .s_axi_wstrb  (s_axi_wstrb),
+      .s_axi_wlast  (s_axi_wlast),
+      .s_axi_bvalid (s_axi_bvalid),
+      .s_axi_bready (s_axi_bready),
+      .s_axi_bid    (s_axi_bid),
+      .s_axi_bresp  (s_axi_bresp),
+
+      .s_axi_arvalid(s_axi_arvalid),
+      .s_axi_arready(s_axi_arready),
+      .s_axi_arid   (s_axi_arid),
+      .s_axi_araddr (s_axi_araddr),
+      .s_axi_arlen  (s_axi_arlen),
+      .s_axi_arsize (s_axi_arsize),
+      .s_axi_arburst(s_axi_arburst),
+      .s_axi_rvalid (s_axi_rvalid),
+      .s_axi_rready (s_axi_rready),
+      .s_axi_rid    (s_axi_rid),
+      .s_axi_rdata  (s_axi_rdata),
+      .s_axi_rresp  (s_axi_rresp),
+      .s_axi_rlast  (s_axi_rlast),
+
+      .sram_cmd_valid   (sram_cmd_valid),
+      .sram_cmd_ready   (sram_cmd_ready),
+      .sram_cmd_wr_en   (sram_cmd_wr_en),
+      .sram_cmd_addr    (sram_cmd_addr),
+      .sram_cmd_meta    (sram_cmd_meta),
+      .sram_cmd_last    (sram_cmd_last),
+      .sram_cmd_wr_data (sram_cmd_wr_data),
+      .sram_cmd_wr_strb (sram_cmd_wr_strb),
+      .sram_resp_valid  (sram_resp_valid),
+      .sram_resp_ready  (sram_resp_ready),
+      .sram_resp_meta   (sram_resp_meta),
+      .sram_resp_last   (sram_resp_last),
+      .sram_resp_rd_data(sram_resp_rd_data)
+  );
+endmodule
+`endif
