4cdea714f4dd2d8ca672021fb540b179b2fbab8b
Patrick Bozeman <pbozeman@gmail.com>
2025-02-02 03:10:53 -0800

    [axi_stripe] improve _wr througput
    It's now a write every clock when pipelining, and only 2 cycles of
    extra latency for the first bvalid to be returned (plus the 1
    unavoidable cycle for the actual write at the sub.) One cycle is for
    pipelining the stripe calculations and routing to the subs, and the
    other is for muxing and registering the b channel response.

--- rtl/svc_axi_stripe_wr.sv
+++ rtl/svc_axi_stripe_wr.sv
@@ -4,6 +4,7 @@
 `include "svc.sv"
 `include "svc_axi_stripe_ax.sv"
 `include "svc_skidbuf.sv"
+`include "svc_sync_fifo.sv"
 `include "svc_unused.sv"
 
 // Stripe write requests from 1 manager to N subordinates based on the low
@@ -84,62 +85,86 @@ module svc_axi_stripe_wr #(
   localparam S_WIDTH = $clog2(NUM_S);
   localparam O_WIDTH = $clog2(AXI_STRB_WIDTH);
 
-  logic                          s_axi_awready_next;
-  logic                          s_axi_bvalid_next;
-  logic [     IW-1:0]            s_axi_bid_next;
-  logic [        1:0]            s_axi_bresp_next;
+  logic                                 sb_s_awvalid;
+  logic [AXI_ADDR_WIDTH-1:0]            sb_s_awaddr;
+  logic [  AXI_ID_WIDTH-1:0]            sb_s_awid;
+  logic [               7:0]            sb_s_awlen;
+  logic [               2:0]            sb_s_awsize;
+  logic [               1:0]            sb_s_awburst;
+  logic                                 sb_s_awready;
 
-  logic [  NUM_S-1:0]            aw_stripe_valid;
-  logic [  NUM_S-1:0][  SAW-1:0] aw_stripe_addr;
-  logic [  NUM_S-1:0][      7:0] aw_stripe_len;
-  logic [S_WIDTH-1:0]            aw_stripe_start_idx;
-  logic [S_WIDTH-1:0]            aw_stripe_end_idx;
+  logic                                 s_axi_bvalid_next;
+  logic [            IW-1:0]            s_axi_bid_next;
+  logic [               1:0]            s_axi_bresp_next;
 
-  logic                          sb_s_wvalid;
-  logic [     DW-1:0]            sb_s_wdata;
-  logic [  STRBW-1:0]            sb_s_wstrb;
-  logic                          sb_s_wready;
+  logic [         NUM_S-1:0]            aw_stripe_valid;
+  logic [         NUM_S-1:0][  SAW-1:0] aw_stripe_addr;
+  logic [         NUM_S-1:0][      7:0] aw_stripe_len;
+  logic [       S_WIDTH-1:0]            aw_stripe_start_idx;
+  logic [       S_WIDTH-1:0]            aw_stripe_end_idx;
 
-  logic [S_WIDTH-1:0]            w_idx;
-  logic [S_WIDTH-1:0]            w_idx_next;
+  logic                                 sb_s_wvalid;
+  logic [            DW-1:0]            sb_s_wdata;
+  logic [         STRBW-1:0]            sb_s_wstrb;
+  logic                                 sb_s_wready;
 
-  logic [        7:0]            w_remaining;
-  logic [        7:0]            w_remaining_next;
+  logic [       S_WIDTH-1:0]            w_idx;
+  logic [       S_WIDTH-1:0]            w_idx_next;
 
-  logic                          w_done;
-  logic                          w_done_next;
+  logic [               7:0]            w_remaining;
+  logic [               7:0]            w_remaining_next;
 
-  logic [  NUM_S-1:0]            m_axi_awvalid_next;
-  logic [  NUM_S-1:0][   IW-1:0] m_axi_awid_next;
-  logic [  NUM_S-1:0][  SAW-1:0] m_axi_awaddr_next;
-  logic [  NUM_S-1:0][      7:0] m_axi_awlen_next;
-  logic [  NUM_S-1:0][      2:0] m_axi_awsize_next;
-  logic [  NUM_S-1:0][      1:0] m_axi_awburst_next;
+  logic                                 w_done;
+  logic                                 w_done_next;
 
-  logic [  NUM_S-1:0]            m_axi_wvalid_next;
-  logic [  NUM_S-1:0][   DW-1:0] m_axi_wdata_next;
-  logic [  NUM_S-1:0][STRBW-1:0] m_axi_wstrb_next;
-  logic [  NUM_S-1:0]            m_axi_wlast_next;
+  logic [         NUM_S-1:0]            m_axi_awvalid_next;
+  logic [         NUM_S-1:0][   IW-1:0] m_axi_awid_next;
+  logic [         NUM_S-1:0][  SAW-1:0] m_axi_awaddr_next;
+  logic [         NUM_S-1:0][      7:0] m_axi_awlen_next;
+  logic [         NUM_S-1:0][      2:0] m_axi_awsize_next;
+  logic [         NUM_S-1:0][      1:0] m_axi_awburst_next;
 
-  logic [  NUM_S-1:0]            b_done;
-  logic [  NUM_S-1:0]            b_done_next;
+  logic [         NUM_S-1:0]            m_axi_wvalid_next;
+  logic [         NUM_S-1:0][   DW-1:0] m_axi_wdata_next;
+  logic [         NUM_S-1:0][STRBW-1:0] m_axi_wstrb_next;
+  logic [         NUM_S-1:0]            m_axi_wlast_next;
 
-  logic [S_WIDTH-1:0]            b_stripe_end_idx;
-  logic [S_WIDTH-1:0]            b_stripe_end_idx_next;
+  logic [         NUM_S-1:0]            b_done;
+  logic [         NUM_S-1:0]            b_done_next;
+
+  logic [       S_WIDTH-1:0]            b_stripe_end_idx;
+  logic [         NUM_S-1:0]            b_stripe_valid;
+
+  logic                                 fifo_b_w_full;
+  logic                                 fifo_b_r_empty;
 
   //-------------------------------------------------------------------------
   //
   // AW channel
   //
   //-------------------------------------------------------------------------
+  svc_skidbuf #(
+      .DATA_WIDTH(AXI_ID_WIDTH + AXI_ADDR_WIDTH + 8 + 3 + 2)
+  ) svc_skidbuf_ar_i (
+      .clk(clk),
+      .rst_n(rst_n),
+      .i_valid(s_axi_awvalid),
+      .i_data({
+        s_axi_awid, s_axi_awaddr, s_axi_awlen, s_axi_awsize, s_axi_awburst
+      }),
+      .o_ready(s_axi_awready),
+      .i_ready(sb_s_awready),
+      .o_data({sb_s_awid, sb_s_awaddr, sb_s_awlen, sb_s_awsize, sb_s_awburst}),
+      .o_valid(sb_s_awvalid)
+  );
 
   svc_axi_stripe_ax #(
       .NUM_S         (NUM_S),
       .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
       .AXI_DATA_WIDTH(AXI_DATA_WIDTH)
   ) svc_axi_stripe_addr_i (
-      .s_addr         (s_axi_awaddr),
-      .s_len          (s_axi_awlen),
+      .s_addr         (sb_s_awaddr),
+      .s_len          (sb_s_awlen),
       .start_idx      (aw_stripe_start_idx),
       .end_idx        (aw_stripe_end_idx),
       .alignment_error(),
@@ -156,36 +181,23 @@ module svc_axi_stripe_wr #(
     m_axi_awsize_next  = m_axi_awsize;
     m_axi_awburst_next = m_axi_awburst;
 
-    if (s_axi_awvalid && s_axi_awready) begin
+    if (sb_s_awvalid && sb_s_awready) begin
       m_axi_awvalid_next = aw_stripe_valid;
       m_axi_awaddr_next  = aw_stripe_addr;
       m_axi_awlen_next   = aw_stripe_len;
 
-      m_axi_awsize_next  = {NUM_S{s_axi_awsize}};
-      m_axi_awburst_next = {NUM_S{s_axi_awburst}};
-      m_axi_awid_next    = {NUM_S{s_axi_awid}};
+      m_axi_awsize_next  = {NUM_S{sb_s_awsize}};
+      m_axi_awburst_next = {NUM_S{sb_s_awburst}};
+      m_axi_awid_next    = {NUM_S{sb_s_awid}};
     end
   end
 
-  always_comb begin
-    s_axi_awready_next = s_axi_awready;
-
-    if (s_axi_awready && s_axi_awvalid) begin
-      s_axi_awready_next = 1'b0;
-    end
-
-    // TODO: try to remove this
-    if (s_axi_bvalid && s_axi_bready) begin
-      s_axi_awready_next = 1'b1;
-    end
-  end
+  assign sb_s_awready = !fifo_b_w_full;
 
   always_ff @(posedge clk) begin
     if (!rst_n) begin
-      s_axi_awready <= 1'b1;
       m_axi_awvalid <= '0;
     end else begin
-      s_axi_awready <= s_axi_awready_next;
       m_axi_awvalid <= m_axi_awvalid_next;
     end
   end
@@ -228,9 +240,9 @@ module svc_axi_stripe_wr #(
     w_done_next       = w_done;
 
     // start of burst
-    if (s_axi_awready && s_axi_awvalid) begin
+    if (sb_s_awready && sb_s_awvalid) begin
       w_idx_next       = aw_stripe_start_idx;
-      w_remaining_next = s_axi_awlen;
+      w_remaining_next = sb_s_awlen;
       w_done_next      = 1'b0;
 
       // w rose before or with aw, so we need to use the new values
@@ -243,17 +255,17 @@ module svc_axi_stripe_wr #(
           m_axi_wvalid_next[aw_stripe_start_idx] = 1'b1;
           m_axi_wdata_next[aw_stripe_start_idx] = sb_s_wdata;
           m_axi_wstrb_next[aw_stripe_start_idx] = sb_s_wstrb;
-          m_axi_wlast_next[aw_stripe_start_idx] = (s_axi_awlen < NUM_S ? '1 :
+          m_axi_wlast_next[aw_stripe_start_idx] = (sb_s_awlen < NUM_S ? '1 :
                                                    '0);
 
           // since NUM_S is a power of 2, we don't have to check against
           // a max value, we can just wrap
           w_idx_next = aw_stripe_start_idx + 1;
 
-          if (s_axi_awlen == 0) begin
+          if (sb_s_awlen == 0) begin
             w_done_next = 1'b1;
           end else begin
-            w_remaining_next = s_axi_awlen - 1;
+            w_remaining_next = sb_s_awlen - 1;
           end
         end
       end
@@ -306,65 +318,61 @@ module svc_axi_stripe_wr #(
   //
   //-------------------------------------------------------------------------
 
-  // I considered using &m_axi_bvalid ? '1 : '0 to drive all the breadys at
-  // the same time, somewhat simplifying this logic. However, doing so will
-  // stall the b channel on the earlier subordinates, potentially preventing
-  // interleaved usage with some other manager when used with an arbiter. This
-  // implementation is slightly more complicated, but provides better
-  // aggregate throughput.
+  // keep track of final idx and count at the time of aw submission
+  svc_sync_fifo #(
+      .DATA_WIDTH(S_WIDTH + NUM_S)
+  ) svc_sync_fifo_b_i (
+      .clk        (clk),
+      .rst_n      (rst_n),
+      .w_inc      (sb_s_awvalid && sb_s_awready),
+      .w_data     ({aw_stripe_end_idx, aw_stripe_valid}),
+      .w_full     (fifo_b_w_full),
+      .w_half_full(),
+      .r_inc      (s_axi_bvalid && s_axi_bready),
+      .r_data     ({b_stripe_end_idx, b_stripe_valid}),
+      .r_empty    (fifo_b_r_empty)
+  );
 
-  // We're always ready to accept responses since we buffer them for our
-  // caller.
-  assign m_axi_bready = '1;
+  // we don't want to complete, and possibly lose, transactions
+  // from the next burst
+  assign m_axi_bready = (b_stripe_valid & {NUM_S{s_axi_bready}});
 
-  // done and end idx tracking
-  for (genvar i = 0; i < NUM_S; i++) begin : gen_b
-    always @(*) begin
-      b_done_next[i]        = b_done[i];
-      b_stripe_end_idx_next = b_stripe_end_idx;
+  // sub b done tracking.
+  //
+  // I considered using &m_axi_bvalid ? '1 : '0 to drive s_axi_bvalid
+  // and m_axi_bready all at the same time , somewhat simplifying this logic.
+  // However, doing so will stall the b channel on the earlier subordinates,
+  // potentially preventing interleaved usage with some other manager when
+  // used with an arbiter. This implementation is slightly more complicated,
+  // but provides better aggregate throughput.
+  always_comb begin
+    b_done_next = b_done;
 
-      if (s_axi_bvalid && s_axi_bready) begin
-        b_done_next[i] = 1'b0;
-      end
-
-      if (s_axi_awready && s_axi_awvalid) begin
-        // if subs aren't participating in the write, consider them done
-        b_done_next[i]        = ~aw_stripe_valid[i];
-        b_stripe_end_idx_next = aw_stripe_end_idx;
-      end
-
-      if (m_axi_bvalid[i] && m_axi_bready[i]) begin
-        b_done_next[i] = 1'b1;
-      end
+    if (s_axi_bvalid && s_axi_bready) begin
+      b_done_next = '0;
     end
+
+    b_done_next = b_done_next | (m_axi_bvalid & m_axi_bready);
   end
 
   // b channel back to the caller
-  //
-  // TODO: s_axi_bvalid has a cycle of latency that could be optimized out.
-  // When doing this remember that the signal needs to be registered with no
-  // combinatorial logic on the final output, which is the tricky bit given
-  // the logic up to this point. A counter might work, at the expense of
-  // a bunch of extra gates. Or there might be some way to tie it directly to
-  // the final stripe's bvalid given that we wrote in order. Get formal
-  // verification running first and then revisit this.
-  always @(*) begin
+  always_comb begin
     s_axi_bvalid_next = s_axi_bvalid && !s_axi_bready;
     s_axi_bid_next    = s_axi_bid;
     s_axi_bresp_next  = s_axi_bresp;
 
-    // Note: we can only do this because we don't accept new writes
-    // until the b channel has been accepted too. Otherwise, we would need to
-    // store pending b channel info in case the last one wasn't accepted.
     if (m_axi_bvalid[b_stripe_end_idx] && m_axi_bready[b_stripe_end_idx]) begin
       s_axi_bid_next   = m_axi_bid[b_stripe_end_idx];
       s_axi_bresp_next = m_axi_bresp[b_stripe_end_idx];
     end
 
-    // Similar to the above, all requests should start with bvalid low since
-    // we wait on it to be awready. Hence, we don't need the usual
-    // if (!s_axi_bvalid || s_axi_bready) check here.
-    s_axi_bvalid_next = &b_done_next;
+    if (!s_axi_bvalid || s_axi_bready) begin
+      if (fifo_b_r_empty) begin
+        s_axi_bvalid_next = 1'b0;
+      end else begin
+        s_axi_bvalid_next = &(b_done_next | ~b_stripe_valid);
+      end
+    end
   end
 
   always_ff @(posedge clk) begin
@@ -378,13 +386,12 @@ module svc_axi_stripe_wr #(
   end
 
   always_ff @(posedge clk) begin
-    s_axi_bid        <= s_axi_bid_next;
-    s_axi_bresp      <= s_axi_bresp_next;
-    b_stripe_end_idx <= b_stripe_end_idx_next;
+    s_axi_bid   <= s_axi_bid_next;
+    s_axi_bresp <= s_axi_bresp_next;
   end
 
-  `SVC_UNUSED({s_axi_awaddr[S_WIDTH+O_WIDTH-1:O_WIDTH], s_axi_wlast,
-               m_axi_bid[NUM_S-1:1], m_axi_bresp[NUM_S-1:1]});
+  `SVC_UNUSED({sb_s_awaddr[S_WIDTH+O_WIDTH-1:O_WIDTH], s_axi_wlast,
+               m_axi_bid[NUM_S-1:1], m_axi_bresp[NUM_S-1:1], fifo_b_r_empty});
 
 `ifdef FORMAL
   // Formal testing happens in the combined module, but we do have asserts
@@ -405,10 +412,8 @@ module svc_axi_stripe_wr #(
         assert (int'(s_axi_awsize) == $clog2(AXI_DATA_WIDTH / 8));
       end
 
-      if (s_axi_awvalid && s_axi_awready) begin
-        // See the comment in the b-channel section. Given the current
-        // implementation, it would be a bug if bvalid is high here.
-        assert (!s_axi_bvalid);
+      if (|m_axi_bvalid) begin
+        assert (!fifo_b_r_empty);
       end
     end
   end
