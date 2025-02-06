fcd7136dd172b68a089527e2fd4e80073b0267af
Patrick Bozeman <pbozeman@gmail.com>
2025-02-06 14:58:34 -0800

    [gfx_fb] pipeline fb writes

--- rtl/svc_gfx_fb.sv
+++ rtl/svc_gfx_fb.sv
@@ -2,6 +2,7 @@
 `define SVC_GFX_FB_SV
 
 `include "svc.sv"
+`include "svc_sync_fifo_n1.sv"
 `include "svc_unused.sv"
 
 // Takes a gfx interface (x, y, pixel) and stores it in an axi backed frame
@@ -44,30 +45,56 @@ module svc_gfx_fb #(
     input  logic [               1:0] m_axi_bresp,
     output logic                      m_axi_bready
 );
-  // TODO: pipeline this. As is, this is just asking for timing troubles.
-  // (Maybe just use a registered sb and do the calc on the way in. This would
-  // remove the combo logic on gfx_ready too.)
-  logic [AXI_ADDR_WIDTH-1:0] fb_addr;
-  assign fb_addr       = h_visible * s_gfx_y + AXI_ADDR_WIDTH'(s_gfx_x);
+  logic                      aw_ready;
+  logic                      w_ready;
 
-  assign m_axi_awvalid = s_gfx_valid;
-  assign m_axi_awaddr  = fb_addr;
+  logic [AXI_ADDR_WIDTH-1:0] fb_addr;
+  assign fb_addr     = h_visible * s_gfx_y + AXI_ADDR_WIDTH'(s_gfx_x);
+
+  // split the gfx stream into an AW and W stream with a fifo for each
+  assign s_gfx_ready = aw_ready && w_ready;
+
+  svc_sync_fifo_n1 #(
+      .DATA_WIDTH(AXI_ADDR_WIDTH)
+  ) svc_sync_fifo_n1_aw_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .w_inc   (s_gfx_valid && s_gfx_ready),
+      .w_data  (fb_addr),
+      .w_full_n(aw_ready),
+
+      .r_inc    (m_axi_awvalid && m_axi_awready),
+      .r_data   (m_axi_awaddr),
+      .r_empty_n(m_axi_awvalid)
+  );
+
+  svc_sync_fifo_n1 #(
+      .DATA_WIDTH(AXI_DATA_WIDTH)
+  ) svc_sync_fifo_n1_w_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .w_inc   (s_gfx_valid && s_gfx_ready),
+      .w_data  (AXI_DATA_WIDTH'(s_gfx_pixel)),
+      .w_full_n(w_ready),
+
+      .r_inc    (m_axi_wvalid && m_axi_wready),
+      .r_data   (m_axi_wdata),
+      .r_empty_n(m_axi_wvalid)
+  );
+
   assign m_axi_awid    = 0;
   assign m_axi_awlen   = 0;
   assign m_axi_awsize  = `SVC_MAX_AXSIZE(AXI_DATA_WIDTH);
   assign m_axi_awburst = 2'b01;
 
-  assign m_axi_wvalid  = s_gfx_valid;
-  assign m_axi_wdata   = AXI_DATA_WIDTH'(s_gfx_pixel);
   assign m_axi_wstrb   = '1;
   assign m_axi_wlast   = 1'b1;
 
   // We're always ready for a response...because we ignore it ¯\_(ツ)_/¯)
   assign m_axi_bready  = 1'b1;
 
-  assign s_gfx_ready   = (m_axi_awready && m_axi_wready);
-
   `SVC_UNUSED({clk, rst_n, v_visible, m_axi_bvalid, m_axi_bid, m_axi_bresp});
-
 endmodule
 `endif
