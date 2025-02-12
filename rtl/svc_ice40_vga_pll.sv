a5b08484c5a7dfb1b155d37597c9904338b8d3d1
Patrick Bozeman <pbozeman@gmail.com>
2025-02-11 18:23:08 -0800

    [ice40][vga] remove redundant gb in pll

--- rtl/svc_ice40_vga_pll.sv
+++ rtl/svc_ice40_vga_pll.sv
@@ -13,8 +13,8 @@ module svc_ice40_vga_pll (
 `ifndef VERILATOR
   logic pll_lock;
 
-  // intermediate clock, see global buffer comment below
-  logic clk_int;
+  // An intermediate clock to pass to a GB is not necessary because this is
+  // using PLLOUTGLOBAL, which already puts the signal on a global buffer
 
   SB_PLL40_CORE #(
       .FEEDBACK_PATH("SIMPLE"),
@@ -27,20 +27,7 @@ module svc_ice40_vga_pll (
       .RESETB      (1'b1),
       .BYPASS      (1'b0),
       .REFERENCECLK(clk_i),
-      .PLLOUTGLOBAL(clk_int)
-  );
-
-  // Hook up PLL output to a global buffer
-  //
-  // From: FPGA-TN-02052-1-4-iCE40-sysCLOCK-PLL-Design-User-Guide.pdf
-  //
-  // "Required for a user’s internally generated FPGA signal
-  // that is heavily loaded and requires global buffering.
-  // For example, a user’s logic-generated clock."
-
-  SB_GB gb_inst (
-      .USER_SIGNAL_TO_GLOBAL_BUFFER(clk_int),
-      .GLOBAL_BUFFER_OUTPUT        (clk_o)
+      .PLLOUTGLOBAL(clk_o)
   );
 `endif
 
