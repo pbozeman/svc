fb7c862cb49eae842173883c91c5229dfbb07007
Patrick Bozeman <pbozeman@gmail.com>
2025-02-14 11:57:14 -0800

    [init] add an enable signal

--- rtl/svc_init.sv
+++ rtl/svc_init.sv
@@ -9,12 +9,13 @@ module svc_init #(
     parameter RST_CYCLES = 65
 ) (
     input  logic clk,
+    input  logic en,
     output logic rst_n = 1'b0
 );
   logic [$clog2(RST_CYCLES)-1:0] cnt = 0;
 
   always_ff @(posedge clk) begin
-    if (cnt < RST_CYCLES) begin
+    if (en && cnt < RST_CYCLES) begin
       cnt   <= cnt + 1;
       rst_n <= 1'b0;
     end else begin
