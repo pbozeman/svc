d8d9b1f5a9d69b94d60196699a1d544032062ff6
Patrick Bozeman <pbozeman@gmail.com>
2025-02-20 18:37:41 -0800

    [svc] no default net none under vivado

--- rtl/svc.sv
+++ rtl/svc.sv
@@ -1,10 +1,19 @@
 `ifndef SVC_SV
 `define SVC_SV
 
-// verilog_format: off
 `timescale 1ns / 1ps
+
+// Vivado doesn't seem to be following the sv standard and forces one
+// to use 'input wire logic <signal>' rather than 'input logic <signal>' when
+// the default net type is none.
+//
+// See:
+//   https://adaptivesupport.amd.com/s/question/0D52E00006iHwnKSAS/vivado-synthesis-error-synth-86735-cause-by-using-defaultnettype-none
+// and
+//   https://stackoverflow.com/questions/52173730/systemverilog-changing-port-type-from-wire-to-logic-gives-error-when-usingn-defa
+`ifdef SVC_DEF_NET_NONE
 `default_nettype none
-// verilog_format: on
+`endif
 
 `define SVC_MAX_AXSIZE(dw) 3'($clog2(dw) - 3)
 
