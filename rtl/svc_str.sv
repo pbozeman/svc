66d6ac160c4501410826aa64fe3447252458d8bc
Patrick Bozeman <pbozeman@gmail.com>
2025-03-22 23:14:41 -0700

    [print] initial implementation

--- rtl/svc_str.sv
+++ rtl/svc_str.sv
@@ -3,16 +3,25 @@
 
 parameter SVC_STR_MAX_LEN = 128;
 
+// verilator lint_off: UNUSEDPARAM
+parameter SVC_STR_WIDTH = 8 * SVC_STR_MAX_LEN;
+// verilator lint_on: UNUSEDPARAM
+
 // convert a literal to a packed string, msb first like a "normal" string
 task automatic svc_str_init(output logic [8*SVC_STR_MAX_LEN-1:0] str,
                             input logic [8*SVC_STR_MAX_LEN-1:0] literal,
                             input int len);
   str = '0;
+  // If this file is included, but the macro is not used in that module,
+  // directly, there will be an UNUSEDLOOP warning, e.g. if a module is used
+  // that uses svc_print, this can happen at the top level. Silence it.
+  // verilator lint_off: UNUSEDLOOP
   for (int i = 0; i < SVC_STR_MAX_LEN; i++) begin
     if (i < len) begin
       str[8*i+:8] = literal[8*(len-1-i)+:8];
     end
   end
+  // verilator lint_on: UNUSEDLOOP
 endtask
 
 // helper macro to avoid having to specify the len
