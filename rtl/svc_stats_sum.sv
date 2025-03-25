0216b3027b495cb436e315561ee945d6881efd9a
Patrick Bozeman <pbozeman@gmail.com>
2025-03-25 15:01:18 -0700

    [stats_sum] change val to only sum

---
+++ rtl/svc_stats_sum.sv
@@ -0,0 +1,36 @@
+`ifndef SVC_STATS_SUM_SV
+`define SVC_STATS_SUM_SV
+
+`include "svc.sv"
+`include "svc_accumulator.sv"
+
+// This is a direct pass through to the accumulator, but can be used
+// along side other stat modules with consistent naming.
+
+module svc_stats_sum #(
+    parameter WIDTH       = 8,
+    parameter STAT_WIDTH  = 32,
+    parameter STAT_STAGES = 4
+) (
+    input logic clk,
+    input logic rst_n,
+
+    input logic             clr,
+    input logic             en,
+    input logic [WIDTH-1:0] val,
+
+    output logic [STAT_WIDTH-1:0] sum
+);
+  svc_accumulator #(
+      .WIDTH (STAT_WIDTH),
+      .STAGES(STAT_STAGES)
+  ) svc_acc_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+      .clr  (clr),
+      .en   (en),
+      .val  (STAT_WIDTH'(val)),
+      .acc  (sum)
+  );
+endmodule
+`endif
