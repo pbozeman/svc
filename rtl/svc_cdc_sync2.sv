eff44b289e403d8f2c5ddbb429203d308f95f467
Patrick Bozeman <pbozeman@gmail.com>
2025-04-07 18:03:27 -0700

    [cdc] set async_reg on sync2

--- rtl/svc_cdc_sync2.sv
+++ rtl/svc_cdc_sync2.sv
@@ -15,18 +15,21 @@ module svc_cdc_sync2 #(
     input  logic             clk,
     input  logic             rst_n,
     input  logic [WIDTH-1:0] d,
-    output logic [WIDTH-1:0] q = 0
+    output logic [WIDTH-1:0] q
 );
 
-  // 1st stage ff output
-  logic [WIDTH-1:0] q1 = 0;
+  (* async_reg = "true" *)logic [WIDTH-1:0] q1 = 0;
+  (* async_reg = "true" *)logic [WIDTH-1:0] q_int = 0;
+
+  assign q = q_int;
 
   always_ff @(posedge clk) begin
     if (!rst_n) begin
-      q  <= 0;
-      q1 <= 0;
+      q_int <= 0;
+      q1    <= 0;
     end else begin
-      {q, q1} <= {q1, d};
+      q_int <= q1;
+      q1    <= d;
     end
   end
 endmodule
