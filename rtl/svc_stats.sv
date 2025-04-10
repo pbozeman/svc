d79d00d1f36b46fa3e085b7df6e1e94d5eb47a6b
Patrick Bozeman <pbozeman@gmail.com>
2025-04-09 23:03:14 -0700

    [ice40] add -DICE40 and use in stats

--- rtl/svc_stats.sv
+++ rtl/svc_stats.sv
@@ -1,11 +1,7 @@
 `ifndef SVC_STATS_SV
 `define SVC_STATS_SV
 
-// TODO: these need tuning. They also should use some form of fpga
-// architecture, rather than the tool being used to synthesize.
-// For now, this is the way the default for an ice40 vs. a
-// xilinx chip is getting set (albeit crudely)
-`ifdef SYNTH_YOSYS
+`ifdef ICE40
 `define SVC_PIPE_BPS 4
 `else
 `define SVC_PIPE_BPS 8
