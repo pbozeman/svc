e4e50d3821ca18ebdbf14457400072d5bded0220
Patrick Bozeman <pbozeman@gmail.com>
2025-10-30 00:37:29 -0700

    [rv] integrate pc

--- rtl/rv/svc_rv.sv
+++ rtl/rv/svc_rv.sv
@@ -2,24 +2,61 @@
 `define SVC_RV_SV
 
 `include "svc.sv"
+`include "svc_unused.sv"
+
 `include "svc_rv_imem.sv"
+`include "svc_rv_pc.sv"
+`include "svc_rv_idec.sv"
 
 module svc_rv #(
+    parameter int XLEN    = 32,
     parameter int IMEM_AW = 10,
 
     // verilog_lint: waive explicit-parameter-storage-type
     parameter IMEM_INIT = ""
 ) (
     input logic clk,
-    input logic rst_n
+    input logic rst_n,
+
+    output logic ebreak
 );
+  logic [XLEN-1:0] pc;
+
+  logic [    31:0] instr;
 
   //
-  // Instruction memory signals
+  // Decoder signals
   //
-  logic               imem_en;
-  logic [IMEM_AW-1:0] imem_addr;
-  // logic [       31:0] imem_data;
+  logic            reg_write;
+  logic            mem_write;
+  logic            alu_a_src;
+  logic [     1:0] alu_b_src;
+  logic [     1:0] alu_op_type;
+  logic [     1:0] res_src;
+  logic [     2:0] imm_type;
+  logic            is_branch;
+  logic            is_jump;
+  logic [     4:0] rd;
+  logic [     4:0] rs1;
+  logic [     4:0] rs2;
+  logic [     2:0] funct3;
+  logic [     6:0] funct7;
+  logic [XLEN-1:0] imm_i;
+  logic [XLEN-1:0] imm_s;
+  logic [XLEN-1:0] imm_b;
+  logic [XLEN-1:0] imm_u;
+  logic [XLEN-1:0] imm_j;
+
+  //
+  // PC
+  //
+  svc_rv_pc #(
+      .XLEN(XLEN)
+  ) pc_i (
+      .clk  (clk),
+      .rst_n(rst_n),
+      .pc   (pc)
+  );
 
   //
   // Instruction memory
@@ -27,16 +64,49 @@ module svc_rv #(
   svc_rv_imem #(
       .AW       (IMEM_AW),
       .INIT_FILE(IMEM_INIT)
-  ) u_imem (
+  ) imem_i (
       .clk  (clk),
       .rst_n(rst_n),
-      .en   (imem_en),
-      .addr (imem_addr),
-      .data ()
+      .en   (1'b1),
+      .addr (pc[IMEM_AW-1+2:2]),
+      .data (instr)
   );
 
-  assign imem_en   = 1'b1;
-  assign imem_addr = '0;
+  //
+  // Instruction decoder
+  //
+  svc_rv_idec #(
+      .XLEN(XLEN)
+  ) idec_i (
+      .instr      (instr),
+      .reg_write  (reg_write),
+      .mem_write  (mem_write),
+      .alu_a_src  (alu_a_src),
+      .alu_b_src  (alu_b_src),
+      .alu_op_type(alu_op_type),
+      .res_src    (res_src),
+      .imm_type   (imm_type),
+      .is_branch  (is_branch),
+      .is_jump    (is_jump),
+      .rd         (rd),
+      .rs1        (rs1),
+      .rs2        (rs2),
+      .funct3     (funct3),
+      .funct7     (funct7),
+      .imm_i      (imm_i),
+      .imm_s      (imm_s),
+      .imm_b      (imm_b),
+      .imm_u      (imm_u),
+      .imm_j      (imm_j)
+  );
+
+  assign ebreak = (instr == 32'h00100073);
+
+  `SVC_UNUSED({pc[XLEN-1:IMEM_AW+2], pc[1:0], reg_write, mem_write, alu_a_src,
+               alu_b_src, alu_op_type, res_src, imm_type, is_branch, is_jump,
+               rd, rs1, rs2, funct3, funct7, imm_i, imm_s, imm_b, imm_u, imm_j
+                });
+
 
 endmodule
 
--- tb/rv/svc_rv_tb.sv
+++ tb/rv/svc_rv_tb.sv
@@ -7,20 +7,88 @@ module svc_rv_tb;
   `TEST_RST_N(clk, rst_n);
 
   localparam int IMEM_AW = 10;
+  logic ebreak;
 
   svc_rv #(
       .IMEM_AW(IMEM_AW)
   ) uut (
-      .clk  (clk),
-      .rst_n(rst_n)
+      .clk   (clk),
+      .rst_n (rst_n),
+      .ebreak(ebreak)
   );
+
+  logic [31:0] MEM[1024];
+  `include "svc_rv_asm.svh"
+
+  `define CHECK_WAIT_FOR_EBREAK(clk) `CHECK_WAIT_FOR(clk, ebreak, 128)
+
+  // Reset assembly state on reset
+  always_ff @(posedge clk) begin
+    if (!rst_n) begin
+      asm_pc <= 0;
+      for (int i = 0; i < 1024; i++) begin
+        MEM[i] <= 32'b0;
+      end
+    end
+  end
+
+  task automatic load_program;
+    int i;
+    for (i = 0; i < 1024; i++) begin
+      uut.imem_i.mem[i] = MEM[i];
+    end
+  endtask
 
   task automatic test_reset;
     `CHECK_EQ(uut.pc, '0);
   endtask
 
+  task automatic test_linear_program;
+    NOP();
+    NOP();
+    NOP();
+    NOP();
+
+    load_program();
+
+    `TICK(clk);
+    `CHECK_EQ(uut.pc, 32'd4);
+
+    `TICK(clk);
+    `CHECK_EQ(uut.pc, 32'd8);
+
+    `TICK(clk);
+    `CHECK_EQ(uut.pc, 32'd12);
+
+    `TICK(clk);
+    `CHECK_EQ(uut.pc, 32'd16);
+  endtask
+
+  task automatic test_ebreak_instruction;
+    EBREAK();
+    NOP();
+
+    load_program();
+
+    // After reset, ebreak should be low
+    `CHECK_FALSE(ebreak);
+
+    `TICK(clk);
+    `CHECK_TRUE(ebreak);
+
+    // We don't currently stop on ebreak
+    `TICK(clk);
+    `CHECK_FALSE(ebreak);
+  endtask
+
+
   `TEST_SUITE_BEGIN(svc_rv_tb);
+
+  // Basic tests
   `TEST_CASE(test_reset);
+  `TEST_CASE(test_linear_program);
+  `TEST_CASE(test_ebreak_instruction);
+
   `TEST_SUITE_END();
 
 endmodule
