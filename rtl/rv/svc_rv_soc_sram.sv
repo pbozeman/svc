66a0c46b361f7792fbd5d88c46cf5e74858dddc1
Patrick Bozeman <pbozeman@gmail.com>
2025-11-01 19:45:47 -0700

    [mem][rv] simplify mem interfaces

--- rtl/rv/svc_rv_soc_sram.sv
+++ rtl/rv/svc_rv_soc_sram.sv
@@ -30,28 +30,21 @@ module svc_rv_soc_sram #(
   //
   logic [31:0] imem_araddr;
   logic        imem_arvalid;
-  logic        imem_arready;
   logic [31:0] imem_rdata;
   logic        imem_rvalid;
-  logic        imem_rready;
 
   //
   // Data memory interface
   //
   logic [31:0] dmem_awaddr;
-  logic        dmem_awvalid;
-  logic        dmem_awready;
   logic [31:0] dmem_wdata;
   logic [ 3:0] dmem_wstrb;
   logic        dmem_wvalid;
-  logic        dmem_wready;
 
   logic [31:0] dmem_araddr;
   logic        dmem_arvalid;
-  logic        dmem_arready;
   logic [31:0] dmem_rdata;
   logic        dmem_rvalid;
-  logic        dmem_rready;
 
   //
   // RISC-V core
@@ -65,23 +58,23 @@ module svc_rv_soc_sram #(
       .rst_n       (rst_n),
       .imem_araddr (imem_araddr),
       .imem_arvalid(imem_arvalid),
-      .imem_arready(imem_arready),
+      .imem_arready(1'b1),
       .imem_rdata  (imem_rdata),
       .imem_rvalid (imem_rvalid),
-      .imem_rready (imem_rready),
+      .imem_rready (),
       .dmem_awaddr (dmem_awaddr),
-      .dmem_awvalid(dmem_awvalid),
-      .dmem_awready(dmem_awready),
+      .dmem_awvalid(),
+      .dmem_awready(1'b1),
       .dmem_wdata  (dmem_wdata),
       .dmem_wstrb  (dmem_wstrb),
       .dmem_wvalid (dmem_wvalid),
-      .dmem_wready (dmem_wready),
+      .dmem_wready (1'b1),
       .dmem_araddr (dmem_araddr),
       .dmem_arvalid(dmem_arvalid),
-      .dmem_arready(dmem_arready),
+      .dmem_arready(1'b1),
       .dmem_rdata  (dmem_rdata),
       .dmem_rvalid (dmem_rvalid),
-      .dmem_rready (dmem_rready),
+      .dmem_rready (),
       .ebreak      (ebreak)
   );
 
@@ -93,21 +86,16 @@ module svc_rv_soc_sram #(
       .AW       (IMEM_AW),
       .INIT_FILE(IMEM_INIT)
   ) imem (
-      .clk    (clk),
-      .rst_n  (rst_n),
-      .araddr (imem_araddr),
-      .arvalid(imem_arvalid),
-      .arready(imem_arready),
-      .rdata  (imem_rdata),
-      .rvalid (imem_rvalid),
-      .rready (imem_rready),
-      .awaddr (32'h0),
-      .awvalid(1'b0),
-      .awready(),
-      .wdata  (32'h0),
-      .wstrb  (4'h0),
-      .wvalid (1'b0),
-      .wready ()
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .rd_addr      (imem_araddr),
+      .rd_valid     (imem_arvalid),
+      .rd_data      (imem_rdata),
+      .rd_data_valid(imem_rvalid),
+      .wr_addr      (32'h0),
+      .wr_data      (32'h0),
+      .wr_strb      (4'h0),
+      .wr_valid     (1'b0)
   );
 
   //
@@ -117,21 +105,16 @@ module svc_rv_soc_sram #(
       .DW(32),
       .AW(DMEM_AW)
   ) dmem (
-      .clk    (clk),
-      .rst_n  (rst_n),
-      .araddr (dmem_araddr),
-      .arvalid(dmem_arvalid),
-      .arready(dmem_arready),
-      .rdata  (dmem_rdata),
-      .rvalid (dmem_rvalid),
-      .rready (dmem_rready),
-      .awaddr (dmem_awaddr),
-      .awvalid(dmem_awvalid),
-      .awready(dmem_awready),
-      .wdata  (dmem_wdata),
-      .wstrb  (dmem_wstrb),
-      .wvalid (dmem_wvalid),
-      .wready (dmem_wready)
+      .clk          (clk),
+      .rst_n        (rst_n),
+      .rd_addr      (dmem_araddr),
+      .rd_valid     (dmem_arvalid),
+      .rd_data      (dmem_rdata),
+      .rd_data_valid(dmem_rvalid),
+      .wr_addr      (dmem_awaddr),
+      .wr_data      (dmem_wdata),
+      .wr_strb      (dmem_wstrb),
+      .wr_valid     (dmem_wvalid)
   );
 
 endmodule
