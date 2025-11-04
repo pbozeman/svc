cb80f25d03c660c395d2967fcef34187f018ff57
Patrick Bozeman <pbozeman@gmail.com>
2025-11-04 02:07:39 -0800

    [rv] tigthen cpi expectations in tests

--- tb/rv/svc_rv_soc_sram_pipelined_bpred_tb.sv
+++ tb/rv/svc_rv_soc_sram_pipelined_bpred_tb.sv
@@ -12,15 +12,17 @@ module svc_rv_soc_sram_pipelined_bpred_tb;
   //
   // CPI expectations with SRAM, pipelined mode, no forwarding, with branch prediction
   //
-  localparam real alu_indep_max_cpi = 1.35;
-  localparam real alu_chain_max_cpi = 3.5;
-  localparam real br_taken_max_cpi = 3.0;
-  localparam real br_not_taken_max_cpi = 2.5;
-  localparam real load_use_max_cpi = 3.5;
-  localparam real mixed_alu_max_cpi = 3.2;
-  localparam real fib12_max_cpi = 1.6;
-  localparam real fib100_max_cpi = 1.6;
-  localparam real bubble_max_cpi = 2.2;
+  // JAL early resolution helps, but lack of forwarding increases stalls
+  //
+  localparam real alu_indep_max_cpi = 1.17;
+  localparam real alu_chain_max_cpi = 2.65;
+  localparam real br_taken_max_cpi = 2.5;
+  localparam real br_not_taken_max_cpi = 2.25;
+  localparam real load_use_max_cpi = 2.25;
+  localparam real mixed_alu_max_cpi = 2.45;
+  localparam real fib12_max_cpi = 1.37;
+  localparam real fib100_max_cpi = 1.34;
+  localparam real bubble_max_cpi = 1.94;
   logic ebreak;
 
   svc_rv_soc_sram #(
