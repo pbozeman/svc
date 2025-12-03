d703ccdd7b501ac480cf6b140a8ba509e84bcb96
Patrick Bozeman <pbozeman@gmail.com>
2025-12-02 22:46:06 -0800

    [mk] use relpaths for rv_f

--- mk/rv_formal.mk
+++ mk/rv_formal.mk
@@ -153,7 +153,7 @@ define rv_f_run_check
 	@touch $(RV_F_BUILD_DIR)/rv_f_run.log $(RV_F_BUILD_DIR)/rv_f_success.log $(RV_F_BUILD_DIR)/rv_f_failure.log
 	@echo "$(RV_F_TGT)" >> $(RV_F_BUILD_DIR)/rv_f_run.log
 	@cd $(RV_F_DIR)/$(RV_F_CFG) && \
-		$(SBY) --prefix $(RV_F_BUILD_DIR)/$(RV_F_TGT) -f $(RV_F_CHK).sby \
+		$(SBY) --prefix $(BUILD_DIR)/rvformal/$(RV_F_TGT) -f $(RV_F_CHK).sby \
 		&& echo "$(RV_F_TGT)" >> $(RV_F_BUILD_DIR)/rv_f_success.log \
 		|| echo "make rv_$(RV_F_TGT)_f" >> $(RV_F_BUILD_DIR)/rv_f_failure.log
 	@touch $(RV_F_BUILD_DIR)/$(RV_F_TGT)/ran
